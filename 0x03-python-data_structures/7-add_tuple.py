097f9a902c3f07025a92e71b58f8e2e52f2bf067
Omodiagbe-Daniel <nosetale2017@gmail.com>
2022-06-06 16:11:03 +0000

    a function that adds 2 tuples

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    tup = 0
+    tupl = 0
+    len1 = len(tuple_a)
+    len2 = len(tuple_b)
+    if len1 > 2 or len2 > 2:
+        tuple_a = tuple_a[0:2]
+        tuple_b = tuple_a[0:2]
+    elif len1 == 1:
+        tuple_a = tuple_a + (0,)
+    elif len1 == 0:
+        tuple_a = tuple_a + (0, 0)
+    if len2 == 1:
+        tuple_b = tuple_b + (0,)
+    elif len2 == 0:
+        tuple_b = tuple_b + (0, 0)
+    for i in range(len(tuple_a)):
+        for j in range(len(tuple_b)):
+            if i == j and i == 0:
+                tupl = tuple_a[i] + tuple_b[i] + tupl
+            elif i == j and i == 1:
+                tup = tuple_a[i] + tuple_b[i] + tup
+    tuple = (tupl, tup)
+    return tuple
+
+
+if __name__ == "__main__":
+    add_tuple()
+
+
+tup1 = add_tuple((2, 3), (5, 6))
+print(tup1)
+tuple_a = (1, 89)
+tuple_b = (88, 11)
+new_tuple = add_tuple(tuple_a, tuple_b)
+print(new_tuple)
+
+print(add_tuple(tuple_a, (1, )))
+print(add_tuple(tuple_a, ()))
