2a5a99d878cf7c098a77ffc45db415de3c4d2174
Omodiagbe-Daniel <nosetale2017@gmail.com>
2022-07-05 23:21:15 +0000

    a class Square that defines a square by: (based on 2-square.py)

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/puthon3
+"""a class Square that defines a square by: (based on 2-square.py)"""
+
+
+class Square:
+    """ a class with private attribute size"""
+
+    def __init__(self, size=0):
+        """instantiation with size of type int"""
+
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
