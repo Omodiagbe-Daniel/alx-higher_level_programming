d9c582798516a155951fc1739a4f83c53013d9ef
Omodiagbe-Daniel <nosetale2017@gmail.com>
2022-07-19 20:17:29 +0000

    proper documentation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -6,7 +6,14 @@ class Square:
     """ a class with private attribute size"""
 
     def __init__(self, size=0):
-        """instantiation with size of type int"""
+        """ instantiation with size of type int.
+
+            Args:
+                size: length of size of square
+
+            raises:
+                TypeError: size must be an integer
+                ValueError: size must be >= 0 """
 
         self.__size = size
         if not isinstance(size, int):
