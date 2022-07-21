656ba51d62106c6d2b470f04b431e98154280bfc
Omodiagbe-Daniel <nosetale2017@gmail.com>
2022-07-21 21:55:03 +0000

    added public instance attribute print_symbol

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -60,7 +60,7 @@ class Rectangle:
             return ""
         else:
             for i in range(self.__height):
-                rect += str((self.print_symbol * self.__width))
+                rect += (str(self.print_symbol) * self.__width))
                 if i != self.__height - 1:
                     rect += ("\n")
             return (rect)
