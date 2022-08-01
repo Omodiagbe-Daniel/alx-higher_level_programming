0ea6183f9ceb606f681e5eca79bb8128c73d8262
Omodiagbe-Daniel <nosetale2017@gmail.com>
2022-08-01 13:27:33 +0000

    a function that prints a square along side it coordinate

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -24,7 +24,7 @@ class Square:
                 print(" " * self.__position[0], end='')
             for j in range(self.__size):
                 print("#", end='')
-            print()
+            print("")
 
     @property
     def size(self):
