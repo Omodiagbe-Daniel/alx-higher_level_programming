79a8b7337209da56aae1cf9141d9f4fbe9569153
Omodiagbe-Daniel <nosetale2017@gmail.com>
2022-08-03 01:24:43 +0000

    made correction to my text file and code

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -5,15 +5,16 @@
 def add_integer(a, b=98):
     """function that adds two integers"""
 
-    if type(a) not in (int, float):
-        raise TypeError("a must be an integer")
-    if type(b) not in (int, float):
-        raise TypeError("b must be an integer")
     if a == float('NaN') or a == float('inf'):
         raise TypeError("a must be an integer")
-    if b == float('NaN') or b == float('inf'):
+    elif b == float('NaN') or b == float('inf'):
         raise TypeError("b must be an integer")
-    return int(a) + int(b)
+    elif type(a) not in (int, float):
+        raise TypeError("a must be an integer")
+    elif type(b) not in (int, float):
+        raise TypeError("b must be an integer")
+    else:
+        return int(a) + int(b)
 
 
 if __name__ == "__main__":
