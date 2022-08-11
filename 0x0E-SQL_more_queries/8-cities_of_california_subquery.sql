d7c23a4cc76c3b3447a4f2bf0ab5e66fd9646521
Omodiagbe-Daniel <nosetale2017@gmail.com>
2022-08-11 13:28:12 +0000

    made correctio

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,5 @@
 -- list all cities of california found in the database
-SELECT id, name FROM cities
-ORDER BY cities.id ASC;
+SELECT id, name
+FROM cities WHERE state_id IN
+	(SELECT id FROM states WHERE name = 'california')
+ORDER BY id ASC;
