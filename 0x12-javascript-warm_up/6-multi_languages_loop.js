54f0a27e2ecc3d916a338657bf6f7844b3144645
Omodiagbe-Daniel <nosetale2017@gmail.com>
2022-09-06 15:46:08 +0000

    looped through an array

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const lists = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const len = lists.length;
+let i = 0;
+while (i < len) {
+  console.log(lists[i]);
+  i++;
+}
