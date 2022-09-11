d56d8537b8c63017d8f107ab7ddd30f8cf81cb2a
Omodiagbe-Daniel <nosetale2017@gmail.com>
2022-09-11 15:29:07 +0000

    made correction to prevent sql interjections

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -12,5 +12,6 @@ if __name__ == "__main__":
     cur.execute("""SELECT * FROM states WHERE name LIKE
                 'N%' ORDER BY states.id ASC""")
     states1 = cur.fetchall()
-    for state in states1:
-        print(state)
+    if states1 is not None:
+        for state in states1:
+            print(state)
