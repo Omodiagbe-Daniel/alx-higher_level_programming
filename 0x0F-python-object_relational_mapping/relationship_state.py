289c92998ff9fb93ce74fc4ccadcc0a1f265194d
Omodiagbe-Daniel <nosetale2017@gmail.com>
2022-09-22 14:13:56 +0000

    sends a GET request to the URL, and displays the body of the response

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -16,7 +16,7 @@ class State(Base):
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False, unique=True)
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state")
+    cities = relationship("City", backref="state", cascade='save-update, merge, delete')
 
 
 if __name__ == "__main__":
