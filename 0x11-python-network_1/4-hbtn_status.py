082044aa55ced6081e42b85140c31521a8529ade
Omodiagbe-Daniel <nosetale2017@gmail.com>
2022-09-24 12:06:50 +0000

    a Python script that fetches using the requests package

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" fetches a URL using the requests package"""
+
+import requests
+
+
+if __name__ == "__main__":
+    r = requests.get('https://alx-intranet.hbtn.io/status')
+    print("Body response")
+    print(f"\t- type {type(r.text)}")
+    print(f"\t- content {r.text}")
