ba7c12a1d4077ad889349c494a2740066a84c4ef
Omodiagbe-Daniel <nosetale2017@gmail.com>
2022-09-24 12:19:06 +0000

    made correction

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -2,8 +2,9 @@
 """ fetches a URL using the requests package"""
 
 import requests
+from sys import argv
 
 
 if __name__ == "__main__":
-    r = requests.get('https://alx-intranet.hbtn.io')
+    r = requests.get(argv[1])
     print(r.headers['X-Request-Id'])
