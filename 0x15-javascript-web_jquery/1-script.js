4a25486c9459aede59236fcda2d343cc1576c073
Omodiagbe-Daniel <nosetale2017@gmail.com>
2022-10-27 00:10:21 +0000

    made corrections

--- 0x15-javascript-web_jquery/1-script.js
+++ 0x15-javascript-web_jquery/1-script.js
@@ -1,8 +1,6 @@
-#!/usr/bin/node
-
 /*
   updating text color in header tag
 */
 
-$('header').css("color: #FF0000)
+$('header').css(color: "#FF0000")
 
---
+++ 0x15-javascript-web_jquery/2-script.js
@@ -0,0 +1,8 @@
+/*
+  updating text color in header tag
+*/
+
+$('#red_header').click(function() {
+  $('header').css({color: "#FF0000"});
+});
+
