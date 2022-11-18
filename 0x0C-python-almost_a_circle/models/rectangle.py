4286a6d9e35c5af21cf102f99874eac234ab8105
Omodiagbe-Daniel <nosetale2017@gmail.com>
2022-11-18 02:57:20 +0000

    handling errors and exceptions

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -12,12 +12,10 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, val):
-        if type(val) not in (int, float):
-            raise TypeError("width must be a number")
-            return
-        if val < 0:
-            raise ValueError("number must be 0 or greater than 0")
-            return
+        if type(val) is not int:
+            raise TypeError("width must be an integer")
+        if val <= 0:
+            raise ValueError("width must be > 0")
         self.__width = val
 
     @property
@@ -26,12 +24,10 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, val):
-        if type(val) not in (int, float):
-            raise TypeError("height must be a number")
-            return
+        if type(val) is not int:
+            raise TypeError("height must be an integer")
         if val < 0:
-            raise ValueError("number must be 0 or greater than 0")
-            return
+            raise ValueError("height must be > 0")
         self.__height = val
 
     @property
@@ -40,12 +36,10 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, val):
-        if type(val) not in (int, float):
-            raise TypeError("x must be a number")
-            return
+        if type(val) is not int:
+            raise TypeError("x must be a integer")
         if val < 0:
-            raise ValueError("number must be 0 or greater than 0")
-            return
+            raise ValueError("x must be >= 0")
         self.__x = val
 
     @property
@@ -54,12 +48,10 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, val):
-        if type(val) not in (int, float):
-            raise TypeError("y must be a number")
-            return
+        if type(val) is not (int):
+            raise TypeError("y must be a integer")
         if val < 0:
-            raise ValueError("number must be 0 or greater than 0")
-            return
+            raise ValueError("y must be >= 0")
         self.__y = val
 
     def __init__(self, width, height, x=0, y=0, id=None):
