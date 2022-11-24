08ca3a5a0fab63edf7e495ccae5fa3638343812e
Omodiagbe-Daniel <nosetale2017@gmail.com>
2022-11-24 01:47:40 +0000

    made correction

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,11 +2,13 @@
 
 
 def print_matrix_integer(matrix=[[]]):
+    x = 0
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
             print("{:d}".format(matrix[i][j]), end=' ')
-        print()
-
+        if (x != len(matrix) - 1):
+            print()
+            x += 1
 
 if __name__ == "__main__":
     print_matrix_integer()
