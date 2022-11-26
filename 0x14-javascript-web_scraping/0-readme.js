62b3e9a581901ab53f4e78c9cd50d28cf9df15c6
Omodiagbe-Daniel <nosetale2017@gmail.com>
2022-11-26 09:24:29 +0000

    a script that reads and prints the content of a file.

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+/*
+  a script that reads and prints the content of a file
+*/
+
+const fs = require('fs');
+fs.readFile(process.argv[2], function (err, data) {
+  if (err) {
+    return console.error(err);
+  }
+  console.log(data.toString());
+})
