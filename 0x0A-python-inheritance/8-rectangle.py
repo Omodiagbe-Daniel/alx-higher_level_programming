4fab99bee5e2f0b70de5c233a319479a6fcd38cc
root <root@OMODIAGBE-DANIEL-ONOSETALE.localdomain>
2022-12-17 11:01:48 +0100

    updated rectangle class

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -27,7 +27,11 @@ class Rectangle(BaseGeometry):
        validated by integer_validator"""
 
     def __init__(self, width, height):
-        self.__width = width
-        self.__height = height
-
-
+        if type(width) is int:
+            self.__width = int(width)
+        else:
+            raise TypeError("width must be an integer")
+        if type(height) is int:
+            self.__height = int(height)
+        else:
+            raise TypeError("height must be an integer")
