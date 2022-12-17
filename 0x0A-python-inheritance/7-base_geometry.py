daa78cc8f1ed379f574b1bb662dad3fd5c7848e9
root <root@OMODIAGBE-DANIEL-ONOSETALE.localdomain>
2022-12-17 11:35:26 +0100

    updated basegeometry

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -13,7 +13,7 @@ class BaseGeometry:
         if type(value) != int:
             raise TypeError(name + " must be an integer")
 
-        if value <= 0:
+        if value < 0:
             raise ValueError(name + " must be greater than 0")
         self.name = name
         self.__value = value
