33ebe49daeb1fc049d2b3563708a74f895505f8b
root <root@OMODIAGBE-DANIEL-ONOSETALE.localdomain>
2022-12-20 23:43:52 +0100

    Python script that takes in a URL, sends a request to the URL and displays the body of the response

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+    Python script that takes in a URL
+    sends a request to the URL
+     displays the body of the response
+"""
+
+if __name__ == "__main__":
+    from sys import argv
+    import requests
+
+    r = requests.get(argv[1])
+    if (r.status_code >= 400):
+        print(f"Error code: {r._status_code}")
+    else:
+        print(r.text)
