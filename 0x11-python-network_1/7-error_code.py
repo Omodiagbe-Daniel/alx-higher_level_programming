1090c47b9b575c1017d00a3463d84cb55fd85eef
root <root@OMODIAGBE-DANIEL-ONOSETALE.localdomain>
2022-12-20 23:46:09 +0100

    Python script that takes in a URL, sends a request to the URL and displays the body of the response

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -11,6 +11,6 @@ if __name__ == "__main__":
 
     r = requests.get(argv[1])
     if (r.status_code >= 400):
-        print(f"Error code: {r._status_code}")
+        print(f"Error code: {r.status_code}")
     else:
         print(r.text)
