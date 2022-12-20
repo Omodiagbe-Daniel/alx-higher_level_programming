2745eda01849d09f5376e4294bd19b6b7ddfa353
root <root@OMODIAGBE-DANIEL-ONOSETALE.localdomain>
2022-12-20 23:13:40 +0100

    made correction

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 """ gets header of a URL using the requests package"""
 
-import requests
-from sys import argv
-
 
 if __name__ == "__main__":
+    import requests
+    from sys import argv
     r = requests.get(argv[1])
     print(r.headers['X-Request-Id'])
