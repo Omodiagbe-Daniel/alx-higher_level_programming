812b1e6cf7fbf6ebaf266554c734f15261f38fa1
root <root@OMODIAGBE-DANIEL-ONOSETALE.localdomain>
2022-12-20 23:05:55 +0100

    made correction

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,6 +6,6 @@ if __name__ == "__main__":
     import requests
 
     r = requests.get('https://alx-intranet.hbtn.io/status')
-    print("Body response")
+    print("Body response:")
     print(f"\t- type: {type(r.text)}")
     print(f"\t- content: {r.text}")
